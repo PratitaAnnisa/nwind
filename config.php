580417a260dd8ca43ae673b39eada1909ac0b97d
PratitaAnnisa <rahmaannisapra@gmail.com>
2026-07-22 00:57:34 +0700

    koneksi

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php   
+    // Koneksi ke database
     $host = "localhost";
     $user = "root";
     $pass = "";
     $db   = "nwind";
     
+    // Membuat koneksi ke database
     $konek = mysqli_connect($host, $user, $pass, $db);
+
+    // Mengecek koneksi
     if (!$konek) {
         die("Koneksi database gagal: " . mysqli_connect_error());
     }
